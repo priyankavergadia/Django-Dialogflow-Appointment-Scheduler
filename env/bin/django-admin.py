776b65037ce1d3c5b5aa29035e1268cf07cf570b
pvergadia <priyanka.vergadia@gmail.com>
2019-05-11 15:39:31 -0700

    removed env and venv files

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/pvergadia/Documents/DialogflowProjects/python-docs-samples-master/appengine/standard_python37/django/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/pvergadia/Documents/DialogflowProjects/python-docs-samples-master/appengine/standard_python37/django/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
